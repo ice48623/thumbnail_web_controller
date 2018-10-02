811786f6fe2ef22cbe1e52f209d4e02a7b99eb16
ice48623 <ice48623@gmail.com>
2018-10-03 01:38:33 +0700

    add more params

--- controller.py
+++ controller.py
@@ -1,14 +1,28 @@
 from flask import Flask, request
 import os
 import pika
+import json
 app = Flask(__name__)
 
 @app.route("/add", methods=['POST'])
 def add_job_to_queue():
-    bucket = request.args.get('bucket')
-    filename = request.args.get('filename')
-    add_one(bucket + "-" + filename)
-    return "Added to queue: " + bucket + '/' + filename
+    video_host = os.getenv('STORAGE_HOST','localhost')
+    video_bucket_name = request.args.get('bucket')
+    video_object_name = request.args.get('object')
+    target_host = video_host
+    target_bucket_name = request.args.get('t_bucket')
+    target_object_name = request.args.get('t_object')
+    order = {
+        "video_host": video_host,
+        "video_bucket_name": video_bucket_name,
+        "video_object_name": video_object_name,
+        "gif_host": target_host,
+        "gif_target_bucket_name": target_bucket_name,
+        "gif_target_object_name": target_object_name
+    }
+    j_order = json.dumps(order)
+    add_one(j_order)
+    return j_order
 
 @app.route("/addAll", methods=['POST'])
 def add_all():
